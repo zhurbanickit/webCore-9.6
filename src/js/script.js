a492aa5db6196fdaa0ea537bacfebd3c937bbb37
zhurbanickit <zhurbaa0@gmail.com>
2025-11-19 17:25:22 +0900

    Buttons is ready for work open and close

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,33 @@ document.querySelector(".services_main_button_hidden").onclick = function () {
   document.querySelector(".services_main_button_hidden").style.display = "none" ;
   document.querySelector(".services_main_title_hiden").style.display = "none" ;
 }
+
+document.querySelector(".pair_button").onclick = function () {
+    console.log("click .pair_button")
+    document.querySelector(".repair_brands_button_hidden").style.display = "flex" ;
+    document.querySelector(".pair_button").style.display = "none";
+    document.querySelector(".repair_brands_hidden_red").style.display = "flex" ;
+}
+
+document.querySelector(".repair_brands_button_hidden").onclick = function () {
+  console.log("click .repair_brands_button_hidden")  
+  document.querySelector(".pair_button").style.display = "flex" ;
+  document.querySelector(".repair_brands_button_hidden").style.display = "none" ;
+  document.querySelector(".repair_brands_hidden_red").style.display = "none" ;
+}
+
+
+document.querySelector(".repair_types_button_all").onclick = function () {
+    console.log("click .repair_types_button_all")
+    document.querySelector(".repair_types_button_hidden").style.display = "flex" ;
+    document.querySelector(".repair_types_button_all").style.display = "none";
+    document.querySelector(".repair_types_hidden_green").style.display = "flex" ;
+   
+}
+
+document.querySelector(".repair_types_button_hidden").onclick = function () {
+  console.log("click .repair_types_button_hidden")  
+  document.querySelector(".repair_types_button_all").style.display = "flex" ;
+  document.querySelector(".repair_types_button_hidden").style.display = "none" ;
+  document.querySelector(".repair_types_hidden_green").style.display = "none" ;
+}
