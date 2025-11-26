1a25f634c6761dc15e01883cd1ac224cbedeecd3
zhurbanickit <zhurbaa0@gmail.com>
2025-11-26 19:58:21 +0900

    all changes is done and worked

--- src/js/script.js
+++ src/js/script.js
@@ -25,21 +25,29 @@ document.querySelector('.repair_brands_button_hidden').onclick = function () {
   console.log('click .repair_brands_button_hidden')
   document.querySelector('.pair_button').style.display = 'flex'
   document.querySelector('.repair_brands_button_hidden').style.display = 'none'
-  document.querySelector('.repair_brands_hidden_red').style.display = 'none'
+  document
+    .querySelectorAll('.repair_brands_hidden_red')
+    .forEach((el) => (el.style.display = 'none'))
 }
 
 document.querySelector('.repair_types_button_all').onclick = function () {
   console.log('click .repair_types_button_all')
   document.querySelector('.repair_types_button_hidden').style.display = 'flex'
   document.querySelector('.repair_types_button_all').style.display = 'none'
-  document.querySelector('.repair_types_hidden_green').style.display = 'flex'
+  // document.querySelector('.repair_types_hidden_green').style.display = 'flex'
+  document
+    .querySelectorAll('.repair_types_hidden_green')
+    .forEach((el) => (el.style.display = 'flex'))
 }
 
 document.querySelector('.repair_types_button_hidden').onclick = function () {
   console.log('click .repair_types_button_hidden')
   document.querySelector('.repair_types_button_all').style.display = 'flex'
   document.querySelector('.repair_types_button_hidden').style.display = 'none'
-  document.querySelector('.repair_types_hidden_green').style.display = 'none'
+  // document.querySelector('.repair_types_hidden_green').style.display = 'none'
+  document
+    .querySelectorAll('.repair_types_hidden_green')
+    .forEach((el) => (el.style.display = 'none'))
 }
 
 document.querySelector('.nav_menu_left_burger').onclick = function () {
@@ -48,11 +56,25 @@ document.querySelector('.nav_menu_left_burger').onclick = function () {
   document.querySelector('.slider').style.position = 'fixed'
   document.querySelector('.slider').style.height = '100%'
   // document.querySelector('.slider').style.z-index = '1'
+  document.querySelector('.nav_menu').style.filter = 'blur(6px)'
+  document.querySelector('.services').style.filter = 'blur(6px)'
+  document.querySelector('.repair').style.filter = 'blur(6px)'
+  document.querySelector('.commercial').style.filter = 'blur(6px)'
+  document.querySelector('.buttoms').style.filter = 'blur(6px)'
+  document.querySelector('.prices').style.filter = 'blur(6px)'
+  document.querySelector('.slider').style.filter = 'blur(0)'
 }
 
 document.querySelector('.slider_left_button').onclick = function () {
   console.log('click .slider_left_button')
   document.querySelector('.slider').style.display = 'none'
+  document.querySelector('.nav_menu').style.filter = 'blur(0)'
+  document.querySelector('.services').style.filter = 'blur(0)'
+  document.querySelector('.repair').style.filter = 'blur(0)'
+  document.querySelector('.prices').style.filter = 'blur(0)'
+  document.querySelector('.commercial').style.filter = 'blur(0)'
+  document.querySelector('.buttoms').style.filter = 'blur(0)'
+  document.querySelector('.slider').style.filter = 'none'
 }
 
 // const swiper = new Swiper('.swiper', {
